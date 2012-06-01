e3e726d5442519a499fd16b3738c538f6b0e5661
Frankie <showandtell@Showandtell.local>
2012-06-01 15:31:51 +0200

    Compila, tagliato solo il parse

--- server/request_handler.hpp
+++ server/request_handler.hpp
@@ -15,32 +15,28 @@
 #include <boost/noncopyable.hpp>
 
 namespace http {
-	namespace server3 {
+namespace server3 {
 
-		struct reply;
-		struct request;
+struct reply;
+struct request;
 
-		/// The common handler for all incoming requests.
-		class request_handler
-			  : private boost::noncopyable
-		{
-			public:
-				  /// Construct with a directory containing files to be served.
-				  explicit request_handler(const std::string& doc_root);
+/// The common handler for all incoming requests.
+class request_handler: private boost::noncopyable {
+public:
+	/// Construct with a directory containing files to be served.
+	explicit request_handler(const std::string& doc_root);
 
-				    /// Handle a request and produce a reply.
-				    void handle_request(const request& req, reply& rep);
+	/// Handle a request and produce a reply.
+	void handle_request(const request& req, reply& rep);
 
-			private:
-				      /// The directory containing the files to be served.
-				      std::string doc_root_;
+private:
+	/// The directory containing the files to be served.
+	std::string doc_root_;
 
-				        /// Perform URL-decoding on a string. Returns false if the encoding was
-				        /// invalid.
-				        static bool url_decode(const std::string& in, std::string& out);
-		};
 
-	} // namespace server3
+};
+
+} // namespace server3
 } // namespace http
 
 #endif // HTTP_SERVER3_REQUEST_HANDLER_HPP
