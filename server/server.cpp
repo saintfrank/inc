f91dc7e0f3bded6d55c793ba8ad138fee3844c45
Frankie <showandtell@Showandtell.local>
2012-06-05 00:50:55 +0200

    Client quasi ok. Pensare al server ora.

--- server/server.cpp
+++ server/server.cpp
@@ -2,6 +2,7 @@
 #include <boost/thread/thread.hpp>
 #include <boost/bind.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/lexical_cast.hpp>
 #include <vector>
 
 #define DEBUG_SOCKET
@@ -95,3 +96,34 @@ void server::handle_stop() {
 
 } // namespace server3
 } // namespace http
+
+
+
+
+
+int main(int argc, char* argv[]) {
+	try {
+		// Check command line arguments.
+		if (argc != 4) {
+			std::cerr
+					<< "Usage: http_server <address> <port> <threads>\n";
+			std::cerr << "  For IPv4, try:\n";
+			std::cerr << "    receiver 0.0.0.0 80 1 \n";
+			std::cerr << "  For IPv6, try:\n";
+			std::cerr << "    receiver 0::0 80 1 \n";
+			return 1;
+		}
+
+		// Initialise the server.
+		std::size_t num_threads = boost::lexical_cast < std::size_t > (argv[3]);
+		http::server3::server s(argv[1], argv[2], num_threads);
+
+		// Run the server until stopped.
+		s.run();
+	} catch (std::exception& e) {
+		std::cerr << "exception: " << e.what() << "\n";
+	}
+
+	return 0;
+}
+
